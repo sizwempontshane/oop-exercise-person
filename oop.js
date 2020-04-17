a83db54e8856c2fca3444cfbb72cdc7f79881e5e
SizweSeize <mpontshanesizwe@gmail.com>
2020-04-17 11:22:35 +0200

    first commit

---
+++ oop.js
@@ -0,0 +1,18 @@
+class Person{
+
+    constructor(name, age, gender, interests){
+    this.name = name
+    this.age = age
+    this.gender = gender
+    this.interests = [interests]
+    
+
+    }
+    hello(){
+        console.log('Hello, my name is '+this.name+' and I am '+this.age +' years old. I am a '+this.gender+' and I enjoy '+this.interests+'')
+    }
+}
+
+    let person = new Person('Sanele',27, 'male',['playing soccer', 'watching movies','dancing'])
+    let greeting = person.hello()
+   
