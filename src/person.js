dc70a02ef40ae4238abde096a903401b14806e78
SizweSeize <mpontshanesizwe@gmail.com>
2020-05-05 13:21:21 +0200

    Updated TDD

---
+++ src/person.js
@@ -0,0 +1,20 @@
+class Person {
+
+    constructor(name, age, gender, interests) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.interests = interests;
+
+
+    }
+    hello() {
+        return ('Hello, my name is ' + this.name + ' and I am ' + this.age + ' years old. My interests are ' + this.interests[0] + ', ' + this.interests[1] + ' and ' + this.interests[2]);
+    }
+}
+
+let person = new Person ('Ryan',30,'male',['being a hardarse', 'agile', 'ssd hard drives.'])
+let greeting = person.hello()
+console.log(greeting)
+
+module.exports = { Person }                           
